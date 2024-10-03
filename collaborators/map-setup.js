b3923e668eeb30e3bace944c489f670d62cf4b90
Ignacio Brevis <ignacio.brevis.v@gmail.com>
2024-10-03 15:13:15 +0100

    collaborators updated

--- collaborators/map-setup.js
+++ collaborators/map-setup.js
@@ -16,7 +16,7 @@ var map = L.map('map',{
 var locations = [
     { name: "I. Muga", lat: -33.0472, lng: -71.6127 },
     { name: "P. Sepúlveda", lat: -33.0472, lng: -71.6227 },
-    { name: "S. Rojas", lat: -33.0472, lng: -71.6027 },
+    { name: "S. Rojas", lat: -37.8141, lng: 144.9630 },
     { name: "C. Montoya", lat: 6.2476, lng: -75.5658 },
     { name: "D. Pardo", lat: 43.2630, lng: -2.9350 },
     { name: "F. Caro", lat: 43.0671, lng: -2.4800},
